b77355b243b1c7c003e8282d0a283308aa6633cc
Shubham Latiyan <shubham.latiyan@plus91.in>
2019-07-20 11:54:06 +0530

    sync

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('ci with travis');
+});
+
+const server = app.listen(3000, () => {
+    console.log('App is running on 3000');
+})
+module.exports = server;
